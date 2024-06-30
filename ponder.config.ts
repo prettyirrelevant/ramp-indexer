00dcaeeed0f569e3d7356cb7a578cf57c868b568
Isaac Adewumi <ienioladewumi@gmail.com>
2024-06-30 07:40:11 +0100

    add base sepolia

--- ponder.config.ts
+++ ponder.config.ts
@@ -2,6 +2,7 @@ import { createConfig } from "@ponder/core";
 import { http } from "viem";
 
 import { RampBondingCurveAbi } from "./abis/RampBondingCurveAMM";
+import { baseSepolia } from "viem/chains";
 
 export default createConfig({
   networks: {
@@ -13,6 +14,10 @@ export default createConfig({
       chainId: 252,
       transport: http("https://rpc.frax.com"),
     },
+    baseSepolia: {
+      chainId: 84532,
+      transport: http("https://sepolia.base.org"),
+    },
   },
   contracts: {
     RampCurve: {
@@ -20,6 +25,10 @@ export default createConfig({
       network: {
         fraxtal: { startBlock: 6271421 },
         fraxtalTestnet: { startBlock: 9666828 },
+        baseSepolia: {
+          address: "0xFA598e9Bd1970E0cB42b1e23549A6d5436680b51",
+          startBlock: 11972482,
+        },
       },
       abi: RampBondingCurveAbi,
     },
